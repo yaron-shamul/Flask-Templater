3be59307a7d172ef67c18c66defeefcd9f58f89a
sugarderryfire <sugarderryfire@bitbucket.com>
2020-11-29 21:27:17 +0200

    I added some todo list and explained what is the porpose

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from shutil import copyfile, move, copytree
 import os
 
 
+# Hello Every one who watch this tool, I want to build something that will
+# make it easier to build and use html-templates with Flask. 
+# And so then I attched the template here to the project.
+# I need everything to be automated and clean.. Lets do it :)
+
+# Current TODO List: 29/11/2020
+# add html links edit (inside html files)
+# generate a file that routes the pages
+# create some "how to use" guide
+
+
 def restructure():
 
 	bootstrap_folder = r'C:\Users\Yaron Shamul\Desktop\iPortfolio'
