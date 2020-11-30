cf44c4f1f2980499cbfddb729a7eca5a668e4170
sugarderryfire <sugarderryfire@bitbucket.com>
2020-11-30 18:46:56 +0200

    we can now edit multiple params

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from shutil import copyfile, move, copytree
-import os
+import os, re
+import fileinput, sys
 
 
 # Hello Every one who watch this tool, I want to build something that will
@@ -7,29 +8,80 @@ import os
 # And so then I attched the template here to the project.
 # I need everything to be automated and clean.. Lets do it :)
 
-# Current TODO List: 29/11/2020
+# Current TODO List: 30/11/2020
 # add html links edit (inside html files)
 # generate a file that routes the pages
 # create some "how to use" guide
+# class that have the functions bellow and also have a class integers (the pathes)
+# https://startbootstrap.com/themes
+# https://colorlib.com/wp/template/philosophy/
+# https://themewagon.com/thank-you-for-downloading/?item_id=86980&dl=VDQ3OU4rUldFbDZ6ajBzOVBJVnVobzM2STJsaVZPNUpIcGVING5FQ3N0L05hN0xIQnoyWFhkOFpDb3BVNmEwTw==
 
 
-def restructure():
+def clean_text_from_tag(tag, line):
+	pass 
 
-	bootstrap_folder = r'C:\Users\Yaron Shamul\Desktop\iPortfolio'
-	flask_templated = '{}FLASK-TEMPLATED'.format(bootstrap_folder)
+
+"""
+this function will take care the html to be editable and save you time 
+"""	
+def html_organize(static_folder):
+	#src, href(not a url or '#')
+	for html_file in os.listdir(static_folder):
+		for line in fileinput.input([r'{}\{}'.format(static_folder, html_file)], inplace=True):
+			#start_param_index
+			sspi = (line.find('src="'), line.find('href="'))
+			if new_line := edit_line(line, "src"):
+				line = line.replace(line, new_line)
+			elif new_line := edit_line(line, "href"): # we assume there is no place where two params used
+				line = line.replace(line, new_line)
+					
+			sys.stdout.write(line)			
+
+
+"""
+This one will generically handle the rendering and templating html line by it's param.
+"""
+def edit_line(line, param):
+	start_param_index = line.find('{}="'.format(param))					
+	if start_param_index >= 0: # param appears in the line
 	
+		end_param_content = re.findall('{}="(.*?)"'.format(param), line)[0] 
+		end_param_index = len(end_param_content)
+		new_line = line[:start_param_index] + param + "=\"{{ url_for('static', filename='" + end_param_content + "') }} " + line[start_param_index + end_param_index + 6:]
+
+		
+		return new_line
+		
+
+"""
+build the tree structure that familiar with flask project
+"""
+def restructure(bootstrap_folder):
+
+	flask_templated = '{}FLASK-TEMPLATED'.format(bootstrap_folder)	
 	try:
-	    os.mkdir(flask_templated)
-	    os.mkdir(r'{}\templates'.format(flask_templated))
+		os.mkdir(flask_templated)
+		os.mkdir(r'{}\templates'.format(flask_templated))
 
 	except Exception as e:
-	    print(e)
-	    
+		print(e)
+		
 	else:    
-
 		copytree(bootstrap_folder, r'{}\static'.format(flask_templated))
-
+		
 		for file_name in os.listdir(r'{}\static'.format(flask_templated)):
-			if file_name.endswith('.html'):
+			if file_name.endswith('.html'): # we can assume there is no unusals
 				move(r'{}\static\{}'.format(flask_templated, file_name), r'{}\templates\{}'.format(flask_templated, file_name))
-				
+	
+
+"""The fllow will defined here"""
+def main():
+	bootstrap_folder = r'C:\Users\Yaron Shamul\Desktop\iPortfolio'
+
+	restructure(bootstrap_folder)
+	html_organize(r'{}FLASK-TEMPLATED\templates'.format(bootstrap_folder))
+
+
+if __name__ == '__main__':
+	main()
